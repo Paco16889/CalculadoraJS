92b880a99912ccff04847959d0c68351ea6aead1
Paco16889 <francisco.cobsan@gmail.com>
2025-09-06 03:27:26 +0200

    se crea estructura de carpetas y primeras funciones

---
+++ private/backend.js
@@ -0,0 +1,8 @@
+//aqui comenzaremos creando las funciones de las operaciones empezaremos con las basicas suma y resta
+function suma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return suma(a, -b);
+}
