37d7075e47e5ed0107ed637ac103cfc526730c07
Paco16889 <francisco.cobsan@gmail.com>
2025-09-06 16:39:04 +0200

    primeras lineas del fichero que hará las operaciones

---
+++ private/realizaOperaciones.js
@@ -0,0 +1,21 @@
+//en este fichero haremos las operaciones y gestionaremos la logica de la calculadora
+//las funcioens para las operaciones las sacaremos del fichero backend.js
+//y pasaremos al front los datos para ser impresos en panntalla
+
+let operandoA = null;
+let operandoB = null;
+let operador = null;
+let resultado = null;
+let operacionEnCurso = false;
+
+
+//funcion para manejar la entrada de numeros
+
+//funcion para manejar el operador(+ o -)
+
+//funcion para calcular el resultado
+
+
+ // reseteamos variables
+
+ //devolvemos resultado
