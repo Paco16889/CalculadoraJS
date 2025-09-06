7cb35bf53600e665689586fc6f8d1f67a5fd9554
Paco16889 <francisco.cobsan@gmail.com>
2025-09-06 15:58:44 +0200

    avance en la impresion de nuemros por pantalla

--- public/jsFront/imprimirResultado.js
+++ public/jsFront/imprimirResultado.js
@@ -3,5 +3,8 @@ const pantalla = document.getElementById('pantalla');
 
 
 document.addEventListener('submit', (event) => {
-    
+    event.preventDefault();
+    const numeroAImprimir = document.getElementById('uno').value;
+
+    pantalla.innerHTML=`<p>${numeroAImprimir}</p>`
 })
