e069f4d6f39ea6b0c94148998f9a780c48413627
Paco16889 <francisco.cobsan@gmail.com>
2025-09-08 05:19:34 +0200

    progresos y funciones integradas

--- public/jsFront/backend.js
+++ public/jsFront/backend.js
@@ -7,4 +7,50 @@ function resta(a, b) {
     return suma(a, -b);
 }
 
-export {suma, resta};
+function multiplicacion(a, b){
+    let producto = 0;
+    for (let i = 0; i < b; i++) {
+        producto = suma(producto, a);
+        
+    }
+    return producto;
+}
+
+function division(a, b){
+    let dividendo = a;
+    let divisor = b;
+    let cociente;
+    let resto = dividendo;
+    let cocienteDecimal;
+    for (cociente = 0; resto >= divisor; cociente++) {
+        resto = resta(resto, divisor);
+    }
+    
+    if (resto > 0 ) {
+        resto *= 10;
+        for (cocienteDecimal = 0; resto > 0; cocienteDecimal++) {
+            resto = resta(resto, divisor);
+        }
+        if (resto < 0 ) {
+            cocienteDecimal -=1;
+            resto = Math.abs(resto);
+            resto *= 10;
+        for (let i = cocienteDecimal; resto > 0; i++) {
+            resto = resta(resto, divisor);
+            cocienteDecimal = i;
+        }
+        }///tengo que rejunar en array los digitos decimales pff que liadaaaaa a dormir 
+        ///acuerdate estas ahora ismo que te suma los cocientes decimales 
+        //quieres que cociente decimal sea migual menos uno
+        //otro digito decimal que valga la cuenta y que repita sacando un nuevo elemento de array
+        //pueba for dentro de for un for que de a un array valores que sean cada uno la cuenta del bucle interno
+        let cocientesSeparados = [];
+        cocientesSeparados[0] = cociente;
+        cocientesSeparados[1] = cocienteDecimal;
+        let resultado = cocientesSeparados.join('.');
+        return resultado;
+    }
+    return cociente;
+}
+
+export {suma, resta, multiplicacion, division};
--- public/jsFront/imprimirResultado.js
+++ public/jsFront/imprimirResultado.js
@@ -1,11 +1,23 @@
+import { division, multiplicacion, resta, suma } from "./backend.js";
 
 
 const pantalla = document.getElementById('pantalla');
 
 const botones = document.querySelectorAll('button');
 
+
+
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
+        const botonApretado = boton.textContent.trim();
+        let operandoA;
+        let operandoB;
+        let resultado;
+        if (botonApretado === '+' || botonApretado === '-' || botonApretado === '*' || botonApretado === '/') {
+            console.log('has apretado el operador', boton.id);
+        }
+        
+        
         
         if (boton.id === 'c') {
             pantalla.textContent = '0';
@@ -14,7 +26,7 @@ botones.forEach(boton => {
         
         if(boton.id === 'borrar'){
             console.log(boton.id);
-            if (pantalla.textContent.trim().length === 1 || pantalla.textContent.trim() === 'ERROR') {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === 'ERROR') {
                 pantalla.textContent = '0';
             }else{
                 pantalla.textContent = pantalla.textContent.slice(0, -1);
@@ -24,25 +36,52 @@ botones.forEach(boton => {
         
         
         if (boton.id === 'resultado') {
-            try {
-                pantalla.textContent = eval(pantalla.textContent);
-            } catch (error) {
-                pantalla.textContent = 'ERROR';
+            const operadorApretado = pantalla.textContent.match(/[+\-*/]/)[0];
+            let operandos = pantalla.textContent.split(operadorApretado);
+            operandoA = Number(operandos[0]);
+            operandoB = Number(operandos[1]);
+            switch (operadorApretado) {
+                case '+':
+                    console.log('has enntrado en el case suma del resultado');
+                  
+                    resultado = suma(operandoA, operandoB);
+                    break;
+                case '-':
+                    console.log('has enntrado en el case resta del resultado');
+                    resultado = resta(operandoA, operandoB);
+ 
+                    break;
+                case '*':
+                    console.log('has enntrado en el case multiplicacion del resultado');
+                    resultado = multiplicacion(operandoA, operandoB);
+                    break;
+                case '/':
+                    console.log('has enntrado en el case division del resultado');
+                    resultado = division(operandoA, operandoB)
+                    break;
+            
+                default:
+                    break;
             }
+           // try {
+             //   pantalla.textContent = eval(pantalla.textContent);
+            //} catch (error) {
+              //  pantalla.textContent = 'ERROR';
+            //}
+            pantalla.textContent = resultado;
             return;
         }
-        if (boton.id !== 'borrar') {
-            const botonApretado = boton.textContent;
+        
              if (pantalla.textContent === '0'|| pantalla.textContent === 'ERROR') {
             pantalla.textContent = botonApretado;
         }else{
             pantalla.textContent += botonApretado;
         }
-        }else{
-            pantalla.textContent = pantalla.textContent;
-        }
+        
+            
+        
        
           
 
-    })
-})
+    });
+});
