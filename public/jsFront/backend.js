e194ab9dd90d1acff5bda6632fe9d477acff6f48
Paco16889 <francisco.cobsan@gmail.com>
2025-09-08 16:16:37 +0200

    la calculadora da decimales

--- public/jsFront/backend.js
+++ public/jsFront/backend.js
@@ -22,35 +22,41 @@ function division(a, b){
     let cociente;
     let resto = dividendo;
     let cocienteDecimal;
+    let cocientesDecimales = [];
+    let cocientesSeparados = [];
+    let resultado;
     for (cociente = 0; resto >= divisor; cociente++) {
         resto = resta(resto, divisor);
     }
     
-    if (resto > 0 ) {
-        resto *= 10;
-        for (cocienteDecimal = 0; resto > 0; cocienteDecimal++) {
-            resto = resta(resto, divisor);
-        }
-        if (resto < 0 ) {
-            cocienteDecimal -=1;
-            resto = Math.abs(resto);
+    for (let i = 0; i < 6; i++) {
+        if (resto > 0 ) {
+        
             resto *= 10;
-        for (let i = cocienteDecimal; resto > 0; i++) {
-            resto = resta(resto, divisor);
-            cocienteDecimal = i;
+            for (cocienteDecimal = 0; resto >= divisor; cocienteDecimal++) {
+                resto = resta(resto, divisor);
+            }
+            
+           cocientesDecimales[i] = cocienteDecimal;
+            ///tengo que rejunar en array los digitos decimales pff que liadaaaaa a dormir 
+            ///acuerdate estas ahora ismo que te suma los cocientes decimales 
+            //quieres que cociente decimal sea migual menos uno
+            //otro digito decimal que valga la cuenta y que repita sacando un nuevo elemento de array
+            //pueba for dentro de for un for que de a un array valores que sean cada uno la cuenta del bucle interno
+         
+            
         }
-        }///tengo que rejunar en array los digitos decimales pff que liadaaaaa a dormir 
-        ///acuerdate estas ahora ismo que te suma los cocientes decimales 
-        //quieres que cociente decimal sea migual menos uno
-        //otro digito decimal que valga la cuenta y que repita sacando un nuevo elemento de array
-        //pueba for dentro de for un for que de a un array valores que sean cada uno la cuenta del bucle interno
-        let cocientesSeparados = [];
-        cocientesSeparados[0] = cociente;
-        cocientesSeparados[1] = cocienteDecimal;
-        let resultado = cocientesSeparados.join('.');
-        return resultado;
+
     }
-    return cociente;
+    cocientesSeparados[1] = cocientesDecimales.join('');
+
+    if (cocientesDecimales.lenght === 0) {
+        resultado = cociente;
+    }
+    cocientesSeparados[0] = cociente;
+    resultado = cocientesSeparados.join('.')
+    return resultado;
+
 }
 
 export {suma, resta, multiplicacion, division};
